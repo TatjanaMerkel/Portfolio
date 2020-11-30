2e1fc4ffa104b337e44d20bb39c11dc1c815fd27
Tatjana Merkel <tatjana_merkel@outlook.com>
2020-12-01 00:14:11 +0100

    [OS-1] refactor(main-view): Add navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,36 @@
 import React, { Component } from "react";
-import './Header.css';
+import {
+  Navbar,
+  Nav,
+  Form,
+  FormControl,
+  Button,
+  NavDropdown,
+} from "react-bootstrap";
+import "./Header.css";
 
 export class Header extends Component {
   render() {
     return (
       <header>
-        <div id="navbar" className="sticky" style={{ zIndex: "1000" }}>
-          <a className="active" >
-            Gemüse
-          </a>
-          <a>Obst</a>
-          <a>Schokolade</a>
-        </div>
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="#home">Online Shop</Navbar.Brand>
+          <Nav className="mr-auto">
+            <NavDropdown title="Deine Box" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Gemüse-Box</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Obst-Box</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Mix-Box</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">Rohkost-Box</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.5">Basic-Box</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#vegetable">Gemüse</Nav.Link>
+            <Nav.Link href="#fruits">Früchte</Nav.Link>
+            <Nav.Link href="#tea">Tee</Nav.Link>
+          </Nav>
+          <Form inline>
+            <Button variant="outline-info">Warenkorb</Button>
+          </Form>
+        </Navbar>
       </header>
     );
   }
