1cfa020a1a06299cbf278bf643e8faa518e6ee53
Tatjana Merkel <tatjana_merkel@outlook.com>
2020-12-02 22:27:46 +0100

    [OS-1] feat(main-view):  Return the number of button's click

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,39 +4,37 @@ import "./App.css";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 
-
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
 
-    addShoppingCard() {
-        console.log('Add');
-    }
-
-
-  state = {
-    vegetables: [
-      {
-        id: 1,
-        title: "Papaya",
-        description: "Papayas are great",
-        image:
-          "https://5.imimg.com/data5/GG/CC/VN/SELLER-47385627/fresh-high-quality-papaya-500x500.jpg",
-      },
-      {
-        id: 2,
-        title: "Banana",
-        description: "Tati is great",
-        image:
-          "https://images-na.ssl-images-amazon.com/images/I/61fZ%2BYAYGaL._SX679_.jpg",
-      },
-      {
-        id: 3,
-        title: "Blueberry",
-        description: "Blueberries are great",
-        image:
-          "https://5.imimg.com/data5/RI/NH/MY-50344271/natural-fresh-blueberry-500x500.jpg",
-      },
-    ],
-  };
+      vegetables: [
+        {
+          id: 1,
+          title: "Papaya",
+          description: "Papayas are great",
+          image:
+            "https://5.imimg.com/data5/GG/CC/VN/SELLER-47385627/fresh-high-quality-papaya-500x500.jpg",
+        },
+        {
+          id: 2,
+          title: "Banana",
+          description: "Tati is great",
+          image:
+            "https://images-na.ssl-images-amazon.com/images/I/61fZ%2BYAYGaL._SX679_.jpg",
+        },
+        {
+          id: 3,
+          title: "Blueberry",
+          description: "Blueberries are great",
+          image:
+            "https://5.imimg.com/data5/RI/NH/MY-50344271/natural-fresh-blueberry-500x500.jpg",
+        },
+      ],
+    };
+  }
 
   render() {
     const cards = this.state.vegetables.map((vegetable) => (
@@ -45,11 +43,16 @@ class App extends React.Component {
         <Card.Body>
           <Card.Title>{vegetable["title"]}</Card.Title>
           <Card.Text>{vegetable["description"]}</Card.Text>
-        <div className="text-center">
-            <Button onClick={this.addShoppingCard} variant="primary">Warenkorb hinzufügen</Button>
-        </div>
+          <div className="text-center">
+            <Button
+              onClick={() => this.setState({ count: this.state.count + 1 })}
+              variant="primary"
+            >
+              Warenkorb hinzufügen
+            </Button>
+          </div>
+          <div>Click {this.state.count}</div>
         </Card.Body>
-    
       </Card>
     ));
 
