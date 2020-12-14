cd66c12de719f8fa56712cfd3f4482f930fb126c
Tatjana Merkel <tatjana_merkel@outlook.com>
2020-12-14 02:07:13 +0100

    [OS-1] feat(structure): Use Content component

--- src/components/Root/Root.js
+++ src/components/Root/Root.js
@@ -3,6 +3,7 @@ import { Link, Route, Switch } from "react-router-dom";
 
 import About from "./About";
 import Home from "./Home";
+import Content from "./Content";
 
 const Root = () => {
   return (
@@ -11,10 +12,12 @@ const Root = () => {
       <div>
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
+        <Link to="/products">App</Link>
       </div>
 
       <Switch>
         <Route component={Home} exact path="/" />
+        <Route component={Content} path="/products" />
         <Route component={About} path="/about" />
       </Switch>
     </div>
