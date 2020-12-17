13d7036a684bf98c6ae0fc84dfb30404ad30dbd6
Tatjana Merkel <tatjana_merkel@outlook.com>
2020-12-17 22:12:46 +0100

    [OS-2] feat(add-product): Add basic input form to add-product page

--- src/components/Root/AddProduct/AddProduct.js
+++ src/components/Root/AddProduct/AddProduct.js
@@ -1,10 +1,25 @@
 import React from "react";
 
-
 class AddProduct extends React.Component {
-    render() {
-        return <h1>Add Product</h1>
-    }
-};
+  render() {
+    return (
+      <form>
+        <label>
+          Name:
+          <input type="text" name="name" />
+        </label>
+        <br />
+
+        <label>
+            Description:
+            <input type="text" name="description" />
+        </label>
+        <br />
+
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
 
 export default AddProduct;
