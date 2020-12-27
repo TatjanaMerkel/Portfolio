ff211edf68a37c50ba270d4d0ed260f8e3a5ee96
Tatjana Merkel <tatjana_merkel@outlook.com>
2020-12-27 22:19:54 +0100

    [OS-3] refactor(header): Remove Home and About components

--- src/components/Root/Root.js
+++ src/components/Root/Root.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { Link, Route, Switch } from "react-router-dom";
 
-import About from "../About";
-import Home from "../Home";
 import Content from "../Content";
 import AddProduct from "../AddProduct";
 import ProductList from "../ProductList";
@@ -16,17 +14,13 @@ const Root = () => {
       <Header />
 
       <div>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
         <Link to="/products">App</Link>
         <Link to="/admin/add-product">Add Product</Link>
         <Link to="/admin/list-products">List Products</Link>
       </div>
 
       <Switch>
-        <Route component={Home} exact path="/" />
         <Route component={Content} path="/products" />
-        <Route component={About} path="/about" />
         <Route component={AddProduct} path="/admin/add-product" />
         <Route component={ProductList} path="/admin/list-products" />
         <Route component={EditProduct} path="/admin/edit-product/:id" />
