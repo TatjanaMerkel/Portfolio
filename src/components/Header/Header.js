97354f70c81eddaccd9d2a44529c2de69ced8059
Tatjana Merkel <tatjana_merkel@outlook.com>
2020-12-27 23:34:05 +0100

    [OS-5] feat(header-categories): Move category links to products dropdown

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -16,10 +16,11 @@ export class Header extends Component {
     return (
       <header>
         <Navbar bg="dark" variant="dark">
-          <Navbar.Brand as={Link} to="/">Online Shop</Navbar.Brand>
+          <Navbar.Brand as={Link} to="/">
+            Online Shop
+          </Navbar.Brand>
 
           <Nav className="mr-auto">
-
             <NavDropdown title="Deine Box" id="collasible-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Gemüse-Box</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">Obst-Box</NavDropdown.Item>
@@ -30,17 +31,33 @@ export class Header extends Component {
               <NavDropdown.Item href="#action/3.5">Basic-Box</NavDropdown.Item>
             </NavDropdown>
 
-            <Nav.Link as={Link} to="/products">Gemüse</Nav.Link>
-            <Nav.Link as={Link} to="/products">Obst</Nav.Link>
-            <Nav.Link as={Link} to="/products">Tee</Nav.Link>
+            <NavDropdown title="Produkte">
+              <NavDropdown.Item as={Link} to="/products">
+                Gemüse
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/products">
+                Obst
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/products">
+                Tee
+              </NavDropdown.Item>
+            </NavDropdown>
 
-            <Nav.Link as={Link} to="/admin/add-category">Add Category</Nav.Link>
-            <Nav.Link as={Link} to="/admin/list-categories">List Categories</Nav.Link>
+            <Nav.Link as={Link} to="/admin/add-category">
+              Add Category
+            </Nav.Link>
+            <Nav.Link as={Link} to="/admin/list-categories">
+              List Categories
+            </Nav.Link>
 
-            <Nav.Link as={Link} to="/admin/add-product">Add Product</Nav.Link>
-            <Nav.Link as={Link} to="/admin/list-products">List Products</Nav.Link>
+            <Nav.Link as={Link} to="/admin/add-product">
+              Add Product
+            </Nav.Link>
+            <Nav.Link as={Link} to="/admin/list-products">
+              List Products
+            </Nav.Link>
           </Nav>
-          
+
           <Form inline>
             <Button variant="outline-info">Warenkorb</Button>
           </Form>
