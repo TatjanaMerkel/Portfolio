990c288da1aec6001c7d7b8c3017902a40b56659
Tatjana Merkel <tatjana_merkel@outlook.com>
2020-12-28 01:13:35 +0100

    [OS-5] feat(header-categories): Load products of selected category only

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -14,10 +14,19 @@ class Content extends React.Component {
     };
   }
 
-  async componentDidMount() {
+  async updateProducts(category) {
     const res = await fetch(`http://localhost:3001/products?category=${this.state.category}`);
     const vegetables = await res.json();
     this.setState({vegetables: vegetables});
+  }
+
+  componentDidMount() {
+    this.updateProducts(this.state.category);
+  }
+
+  async componentWillReceiveProps(nextProps) {
+    await this.setState({category: nextProps.category})
+    this.updateProducts(this.state.category);
   }
 
   render() {
