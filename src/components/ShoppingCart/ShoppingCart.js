a4539723328e3aae14af39edc0c359bee00ced6c
Tatjana Merkel <tatjana_merkel@outlook.com>
2021-01-02 23:19:23 +0100

    [OS-6] feat(shopping-cart): List products in ShoppingCart table

--- src/components/ShoppingCart/ShoppingCart.js
+++ src/components/ShoppingCart/ShoppingCart.js
@@ -1,21 +1,32 @@
 import React from "react";
 
 class ShoppingCart extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            values: {
-              name: "asdf"
-            },
-        
-            isSubmitting: false,
-            isError: false,
-          };
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: [],
+
+      isSubmitting: false,
+      isError: false,
+    };
+  }
+
+  async componentDidMount() {
+    const res = await fetch("http://localhost:3001/products/");
+    const products = await res.json();
+    this.setState({ products: products });
+  }
 
   render() {
     return (
-      <div>Hello Kitty</div>
+      <table border="1">
+        {this.state.products.map((product, index) => (
+          <tr>
+            <td>{product.id}</td>
+            <td>{product.name}</td>
+          </tr>
+        ))}
+      </table>
     );
   }
 }
