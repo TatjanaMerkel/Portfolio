693dc6b5867f426ba3480b3c2a2c1c1901d33d5f
Tatjana Merkel <tatjana_merkel@outlook.com>
2021-01-03 02:03:46 +0100

    [OS-6] feat(shopping-cart): Calculate total over all products

--- src/components/ShoppingCart/ShoppingCart.js
+++ src/components/ShoppingCart/ShoppingCart.js
@@ -6,21 +6,28 @@ class ShoppingCart extends React.Component {
     super(props);
     this.state = {
       products: [],
-
+      total: 0,
       isSubmitting: false,
       isError: false,
     };
   }
 
   componentDidMount() {
-    Object.keys(localStorage).forEach(async id => {
+    Object.keys(localStorage).forEach(async (id) => {
       const res = await fetch(`http://localhost:3001/product/${id}`);
       const product = await res.json();
+
+      const product_total = this.calculate_total(product);
+      this.setState({total: this.state.total + product_total})
 
       const products = this.state.products;
       products.push(product);
       this.setState({ products: products });
     });
+  }
+
+  calculate_total(product) {
+    return (product.price / 100) * localStorage.getItem(product.id);
   }
 
   render() {
@@ -42,29 +49,31 @@ class ShoppingCart extends React.Component {
           <th>Total </th>
         </thead>
         <tbody>
-        {this.state.products.map((product, index) => (
-          <tr>
-            <td>
-              <img
-                width="100"
-                height="100"
-                src={product.image}
-                alt=""
-                style={{ "object-fit": "cover" }}
-              />
-            </td>
-            <td>{product.name}</td>
-            <td>{product.price / 100} €</td>
-            <td>{localStorage.getItem(product.id)} </td>
-            <th>{product.price/100 * localStorage.getItem(product.id)} €</th>
-          </tr>
-        ))}
+          {this.state.products.map((product, index) => (
+            <tr>
+              <td>
+                <img
+                  width="100"
+                  height="100"
+                  src={product.image}
+                  alt=""
+                  style={{ "object-fit": "cover" }}
+                />
+              </td>
+              <td>{product.name}</td>
+              <td>{product.price / 100} €</td>
+              <td>{localStorage.getItem(product.id)} </td>
+              <th>{this.calculate_total(product)} €</th>
+            </tr>
+          ))}
         </tbody>
         <tfoot>
           <tr>
             <td></td>
             <td></td>
-            <td>100 €</td>
+            <td></td>
+            <td></td>
+            <th>{this.state.total} €</th>
           </tr>
         </tfoot>
       </table>
